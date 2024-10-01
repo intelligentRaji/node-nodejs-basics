32f6e7c7473038560424410b6c9ab29aafc9e033
intelligentRaji <sergdim7@gmail.com>
2024-10-01 19:41:09 +0200

    feat: fs.list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
-const list = async () => {
-    // Write your code here 
+import { join } from 'path';
+import { readDirectory } from '../utils/read-directory.js';
+import { stdout } from 'process';
+
+const { dirname } = import.meta;
+
+const path = join(dirname, 'files');
+
+const list = async (path) => {
+  const files = await readDirectory(path);
+
+  for (const file of files) {
+    stdout.write(`${file.name}\n`);
+  }
 };
 
-await list();
+await list(path);
