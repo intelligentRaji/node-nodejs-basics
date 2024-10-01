66d98a6d3cdfeafe6de2287549d4bdb14bcecd02
intelligentRaji <sergdim7@gmail.com>
2024-10-01 19:49:42 +0200

    feat: fs.read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
-const read = async () => {
-    // Write your code here 
+import { join } from 'path';
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import { FSError } from '../errors/fs-error.js';
+
+const { dirname } = import.meta;
+
+const path = join(dirname, 'files', 'fileToRead.txt');
+
+const read = async (path) => {
+  const readStream = createReadStream(path);
+
+  readStream.on('error', (error) => {
+    if (error.code === 'ENOENT') {
+      throw new FSError(error);
+    }
+  });
+
+  readStream.pipe(stdout);
 };
 
-await read();
+await read(path);
