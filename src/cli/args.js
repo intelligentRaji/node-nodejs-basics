88c61259591479c0bc54711fb006bf00b4c0ba64
intelligentRaji <sergdim7@gmail.com>
2024-10-01 20:35:02 +0200

    feat: cle:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
-const parseArgs = () => {};
+const parseArgs = () => {
+  const args = process.argv.slice(2);
+
+  const output = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    output.push(`${args[i].replace('--', '')} is ${args[i + 1]}`);
+  }
+
+  console.log(output.join(', '));
+};
 
 parseArgs();
