650f5febbe078ea46cea71d43b7f923b7e588de9
intelligentRaji <sergdim7@gmail.com>
2024-10-01 19:12:14 +0200

    feat: fs.delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
-const remove = async () => {
-    // Write your code here 
+import { join } from 'path';
+import { unlink } from 'fs/promises';
+import { FSError } from '../errors/fs-error.js';
+
+const { dirname } = import.meta;
+
+const path = join(dirname, 'files', 'fileToRemove.txt');
+
+const remove = async (path) => {
+  try {
+    await unlink(path);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new FSError(error);
+    }
+  }
 };
 
-await remove();
+await remove(path);
