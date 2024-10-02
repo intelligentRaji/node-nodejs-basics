73ef6ba5765347c7c4ab1d5a21c8ed242e8a9419
intelligentRaji <sergdim7@gmail.com>
2024-10-02 14:01:18 +0200

    feat: streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
-const transform = async () => {
-    // Write your code here 
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
+class ReverseStream extends Transform {
+  _transform(chunk, encoding, done) {
+    this.push(chunk.toString().split('').reverse().join(''));
+    done();
+  }
+}
+
+const transform = () => {
+  stdin.pipe(new ReverseStream()).pipe(stdout);
+
+  stdin.on('data', (message) => {
+    process.stdout.write(`${message}\n`);
+  });
 };
 
-await transform();
+transform();
